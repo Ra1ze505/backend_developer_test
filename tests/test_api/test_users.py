016fa76b8963126a23733a6c79fd9f8a3d497974
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2022-04-13 22:32:47 +0300

    add test /v1/users/{id}/stats

--- tests/test_api/test_users.py
+++ tests/test_api/test_users.py
@@ -6,22 +6,21 @@ from src.api import app
 
 client: requests.Session = TestClient(app)
 
-anvdev_user = {
-    'id': 28631927,
-    'login': 'anvdev',
-    'name': 'Ivan Titov'
-}
+anvdev_user = {"id": 28631927, "login": "anvdev", "name": "Ivan Titov"}
 
 
 def test_user():
-    response = client.put('/v1/users', json=anvdev_user)
+    response = client.put("/v1/users", json=anvdev_user)
     assert response.status_code == status.HTTP_201_CREATED
 
-    response = client.get('/v1/users')
+    response = client.get("/v1/users")
     assert response.json() == [anvdev_user]
 
     response = client.delete(f'/v1/users/{anvdev_user["id"]}')
     assert response.status_code == status.HTTP_200_OK
 
-    response = client.get('/v1/users')
+    response = client.get("/v1/users")
     assert response.json() == []
+
+    response = client.get(f'/v1/users/{anvdev_user["id"]}/stats')
+    assert response.status_code == status.HTTP_200_OK
